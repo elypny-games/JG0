a91eebd6b23bc68572caa4dea69fe78e54143180
scafAndr <andriei.shuliak@mail.ru>
2019-07-07 13:52:21 +0200

    сделано осн. и доп. панели, частично реализованы сущности

---
+++ src/jg0/Engine.java
@@ -0,0 +1,34 @@
+package jg0;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class Engine implements KeyListener{
+	
+	Window parent;
+
+	
+	Engine(Window parent){
+		
+		this.parent = parent;
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		 
+    }
+ 
+    public void keyReleased(KeyEvent e) {
+                 
+    }
+ 
+    public void keyTyped(KeyEvent e) {
+                 
+    }
+
+    
+    //test
+}
+
+
